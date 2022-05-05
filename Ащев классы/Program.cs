bbfd91da177a4a3f56f98bd5b36c06fb8aa2ba1e
D1gout <fanolkon@gmail.com>
2022-05-05 20:19:17 +0500

    New Project 5.05.22

---
+++ Ащев классы/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Ащев
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "Ащев Д.Ю., группа 1, работа 7, вариант 6";
+            Console.WriteLine("Работа с одномерным массивом");
+
+            Console.WriteLine("");
+            Console.WriteLine("--- 1 объект ---");
+
+            Tarray obj1 = new Tarray();
+
+            Console.WriteLine(obj1.GetStringArray());
+            Console.WriteLine("");
+            Console.WriteLine("Минимальное число:\n" + obj1.LowNumber());
+            Console.WriteLine("Среднее:\n" + obj1.SrNumber());
+
+            Console.WriteLine("");
+            Console.WriteLine("--- 2 объект ---");
+            Console.WriteLine("");
+
+            Console.WriteLine("Введите размер массива: ");
+
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите границы случайного числа: ");
+
+            int[] r = Console.ReadLine().Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+
+            int rS = (int)r[0];
+            int rB = (int)r[1];
+
+            Tarray obj2 = new Tarray(n, rS, rB);
+
+            Console.WriteLine(obj2.GetStringArray());
+            Console.WriteLine("");
+            Console.WriteLine("Минимальное число:\n" + obj2.LowNumber());
+            Console.WriteLine("Среднее:\n" + obj2.SrNumber());
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ащев классы/Tarray.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ащев
+{
+    class Tarray
+    {
+        int[] m;
+        int size;
+        int rMin;
+        int rMax;
+        string text = "";
+
+        public Tarray()
+        {
+            size = 15;
+            rMin = 0;
+            rMax = 100;
+            FillArray();
+            text =  $"Объект создаётся конструктором по умолчанию, размер массива = {size}, диапозон рандома от {rMin} до {rMax}";
+        }
+
+        public Tarray(int n, int rS, int rB)
+        {
+            size = n;
+            rMin = rS;
+            rMax = rB;
+            FillArray();
+            text = $"Объект создаётся конструктором с двумя параметрами, размер массива = {size}, диапозон рандома от {rMin} до {rMax}";
+        }
+
+        void FillArray()
+        {
+            Random Rand = new Random();
+            m = new int[size];
+            for (int i = 0; i < size; i++)
+                m[i] = Rand.Next(rMin, rMax);
+        }
+
+        public string GetStringArray()
+        {
+
+            var str = new StringBuilder();
+            for (int i = 0; i < m.Length; i++)
+                str.Append(m[i] + " ");
+            return $"\n{text}\n" + "Массив чисел:\n" + str.ToString();
+        }
+
+        public int LowNumber()
+        {
+            var L = rMax;
+            for (int i = 0; i < size; i++)
+            {
+                if (L > m[i])
+                    L = m[i];
+            }
+            return L;
+        }
+        public double SrNumber()
+        {
+            var k = 0;
+            double SrNum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                SrNum += m[i];
+                k++;
+            }
+
+            SrNum /= k;
+            SrNum = Math.Round(SrNum, 2);
+            return SrNum;
+        }
+    }
+}
+
